c76471a15071d591e8c0a46ce969db97ddc97637
Harry Banis <132654317+ycs1119@users.noreply.github.com>
2023-05-17 16:45:21 -0700

    modifiy raw endpoit

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,20 @@ const port = 443;
 const productsRouter = require('../routes/productRoutes');
 import { handleError } from '../helpers/handleError';
 
-app.disable('x-powered-by');
+
+var options = {
+  inflate: true,
+  limit: '10000kb',
+  type: 'application/octet-stream'
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.raw({ limit: '10mb' }));
+app.use(bodyParser.raw(options));
 app.use('/api/product', productsRouter);
 
 app.get('/', (req, res) => {
-  console.log('req in index', req.body)
+  console.log('req in index', req)
 })
 
 app.listen(port, () => {
