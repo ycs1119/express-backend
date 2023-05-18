a1dbbc5953fdadf9ed1fd8edf0f4b9079cad2df2
Harry Banis <132654317+ycs1119@users.noreply.github.com>
2023-05-18 04:56:38 -0700

    image parse test

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -1,12 +1,23 @@
 const jotForm = require("jotform");
+jotForm.options({
+  debug: true,
+  apiKey: "1e9a4906f3c1ab0988919bb4a8f2e47d"
+});
+
+interface Answer {
+  cfname?: string,
+  name: string,
+  order: string,
+  selectedField?: string,
+  static: string,
+  text: string,
+  type: string,
+  answer?: string
+}
 
 async function getProduct(req: any, res: any, next: any) {
   try {
     console.log("This is the root path for products");
-    jotForm.options({
-      debug: true,
-      apiKey: "1e9a4906f3c1ab0988919bb4a8f2e47d"
-    });
     let forms = await jotForm.getForms();
     forms = forms.filter((form: any) => { return form.status === "ENABLED" })
     res.status(200).send({
@@ -22,28 +33,15 @@ async function getProduct(req: any, res: any, next: any) {
 
 async function create(req: any, res: any, next: any) {
   try {
-
-    const multer = require("multer");
-    const storage = multer.diskStorage({
-      destination: "public/uploads/",
-      filename: (req: any, _file: any, res: (arg0: null, arg1: string) => void): void => {
-        const uniqueSuffix = Date.now();
-        res(null, uniqueSuffix + ".png");
-      },
+    const submissions = await jotForm.getSubmissions();
+    const recent_sub = submissions.filter((submission: any) => { return submission?.form_id === "231352576902052" })?.[0];
+    const answer = (Object.values(recent_sub?.answers).filter((sub: any) => { return !!sub?.answer })?.[0] as Answer)?.answer;
+    const image_url = JSON.parse(answer || "{}")?.widget_metadata?.value[0]?.url;
+    res.status(201).json({
+      success: true,
+      data: image_url,
+      message: "Welcome to here"
     });
-    console.log('You are Welcome to page', storage);
-    // const upload = multer({ storage: storage });
-    // const data = JSON.parse(upload);
-    // res.status(201).json({
-    //   success: true,
-    //   type: "image",
-    //   title: "image Products",
-    //   message: "Welcome to here"
-    // };
-
-    console.log('...................always-data....................', multer);
-
-
   } catch (error) {
     console.log(req);
     next(error);
