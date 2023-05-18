f6e76d26b8114fde2ab832de2d7422da398abdda
Harry Banis <132654317+ycs1119@users.noreply.github.com>
2023-05-17 18:42:49 -0700

    jotform get test

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -1,9 +1,19 @@
+const jotForm = require("jotform");
+
 async function getProduct(req: any, res: any, next: any) {
   try {
+    console.log(req);
+    jotForm.options({
+      debug: true,
+      apiKey: "1e9a4906f3c1ab0988919bb4a8f2e47d"
+    });
+    let forms = await jotForm.getForms();
+    forms = forms.filter((form: any) => { return form.status === "ENABLED" })
     res.status(200).send({
       status: 200,
       success: true,
       message: "This is the root path for products",
+      data: forms
     });
   } catch (error) {
     next(error);
